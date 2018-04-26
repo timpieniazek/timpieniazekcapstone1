484397b185a54a875ca0e69269c571c39ac25544
Tim Pieniazek <timpieniazek@gmail.com>
2018-04-26 18:38:18 -0400

    Inital commit.

---
+++ src/PigLatin.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PigLatin {
+	
+// Tim Pieniazek, github.com/timpieniazek/timpieniazekcapstone1
+// Week 1 Capstone: Pig Latin, 04/26/2018
+	
+	public static final String[] vowels = {"a", "e", "i", "o", "u"};
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.printf("Welcome to the Pig Latin Translator!%n%n");
+		
+//		StringBuilder userInput = new StringBuilder();
+		String userInput;
+		
+		// While loops allows user to repeat after translating line
+		char cont = 'y';
+		while (cont == 'y') {
+			
+		System.out.print("Enter a line to be translated: ");
+		userInput = scan.nextLine();
+		
+		
+		
+		}
+		
+	}
+	
+	public static 
+
+}
